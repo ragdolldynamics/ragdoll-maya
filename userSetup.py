99fe4ceb0a97a1394edc14ca8a45f794851cb39e
Marcus Ottosson <konstruktion@gmail.com>
2021-01-17 12:30:57 +0000

    Fix userSetup.py This didn't play nice with other userSetup's

--- userSetup.py
+++ userSetup.py
@@ -1,13 +1,28 @@
-import os
+"""Ragdoll Startup Script
 
-# Avoid automatically installing plug-in and menu
-# item with this environment variable.
-if os.getenv("RAGDOLL_NO_AUTOLOAD", False):
-    pass
+The alternative is calling `interactive.install()` yourself.
 
-else:
-    from ragdoll import interactive
-    interactive.install()
-    del interactive
+"""
 
-del os
+
+def init():
+    import os
+
+    # Avoid automatically installing plug-in and menu
+    # item with this environment variable.
+    if os.getenv("RAGDOLL_NO_AUTOLOAD", False):
+        pass
+
+    else:
+        from ragdoll import interactive
+        interactive.install()
+
+
+# Run these from within a function call,
+# to avoid polluting the global namespace
+init()
+
+# Clean up any reference to the call,
+# which would otherwise be accessible from
+# e.g. the Script Editor
+del init

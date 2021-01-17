dea91b930090ae56609c2cddf95b4afb17794949
Marcus Ottosson <konstruktion@gmail.com>
2021-01-17 12:26:00 +0000

    Update deploy script for website

--- web/deploy.ps1
+++ web/deploy.ps1
@@ -1,6 +1,21 @@
+# Usage: .\deploy.ps1 c:\path\to\site
+Write-Host "Building website.."
 rez env -e "PYTHONPATH=$(pwd)/plugins" `
+    python-3.9 `
     git `
     nltk `
     mkdocs `
     libsass `
-    -- mkdocs gh-deploy
+    mkdocs build --site-dir $args[0]
+
+copy-item .\CNAME $args[0]
+copy-item .\.nojekyll $args[0]
+
+Write-Host "Uploading website.."
+pushd $args[0]
+git add --all
+git commit -m "Update website"
+git push
+popd
+
+Write-Host "Success!"

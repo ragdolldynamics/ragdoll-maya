f21759998f1bbb3a1ccde76a27b18fd2d740b21e
Marcus Ottosson <konstruktion@gmail.com>
2021-05-27 10:20:06 +0100

    Remove web2

--- web2/serve.ps1
+++
@@ -1,8 +0,0 @@
-# Usage: ./serve.ps1
-rez env -e "PYTHONPATH=$(pwd)/plugins;$(pwd)/../" `
-    git `
-    nltk `
-    python-3.9 `
-    mkdocs_material-6.1.6 `
-    mkdocs_git_revision_date_plugin==0.3.1 -- `
-    mkdocs serve -a localhost:8002 $args

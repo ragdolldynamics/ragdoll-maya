d08f0cfb8aa1e5075c311aa82bd6a5526cfb6c2c
Marcus Ottosson <konstruktion@gmail.com>
2022-10-28 11:06:49 +0100

    Revert "Merge branch 'renderer'"
    This reverts commit 0dfca5d8ab47e58380d4124cc09e6cce0cf6536f, reversing
    changes made to cafaeb531504f2b40063192a55b1fa006025d5e7.

--- ragdoll/vendor/cmdx_ragdoll.py
+++ ragdoll/vendor/cmdx_ragdoll.py
@@ -1517,17 +1517,12 @@ class DagNode(Node):
             >>> parent | "child"
             |parent|child
 
-            # Stackable too
+            # Stacklable too
             >>> grand =  createNode("transform", "grand", child)
             >>> parent | "child" | "grand"
             |parent|child|grand
 
         """
-
-        # Handle cases where self has a namespace
-        # and no namespace is provided.
-        if ":" not in other:
-            other = "%s:%s" % (self.namespace(), other)
 
         return encode("%s|%s" % (self.path(), other))
 
@@ -4612,7 +4607,7 @@ class Vector(om.MVector):
     """
 
     def __mul__(self, value):
-        return super(Vector, self).__mul__(value)
+        return Vector(super(Vector, self).__mul__(value))
 
     def __add__(self, value):
         if isinstance(value, (int, float)):
